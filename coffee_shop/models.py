f0561951e9092f05ac46038595a929652ce79845
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-22 20:41:07 +0330

    feat: add "Order" and "OrderItem" class model

--- coffee_shop/models.py
+++ coffee_shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    menu_items = models.ManyToManyField(MenuItem, through='OrderItem')
+    status = models.CharField(max_length=20, default='Pending')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
