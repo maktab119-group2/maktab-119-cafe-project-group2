650ff563b30267461bfd072e50f69ab80fb0640d
Zahed <88390908+Zahedhosseini65@users.noreply.github.com>
2025-01-22 21:00:39 +0330

    feat:add def register and user_login

--- coffee_shop/views.py
+++ coffee_shop/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Receipt
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import authenticate, login
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
 
 # Create your views here.
