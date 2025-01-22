a519015ab2f1fd7e3d96508ca51280c8ac499058
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-22 20:45:35 +0330

    feat: add "add_menu_item" and "edit_menu_item" function view

--- coffee_shop/views.py
+++ coffee_shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def add_menu_item(request):
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST, request.FILES)  # Include request.FILES here
+        if form.is_valid():
+            form.save()
+            return redirect('menu')
+    else:
+        form = MenuItemForm()
+    return render(request, 'cafe/add_menu_item.html', {'form': form})
+
+
+def edit_menu_item(request, item_id):
+    item = MenuItem.objects.get(id=item_id)
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST, request.FILES, instance=item)  # Include request.FILES here
+        if form.is_valid():
+            form.save()
+            return redirect('menu')
+    else:
+        form = MenuItemForm(instance=item)
+    return render(request, 'cafe/edit_menu_item.html', {'form': form})
