ff5dd9b1e6c70ed5e4716ae7904557bd0ca1fb51
darya84 <daryalcd1384@gmail.com>
2025-01-23 01:48:06 +0330

    feat: add MenuItem class

--- coffee_shop/models.py
+++ coffee_shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    serving_time_period = models.CharField(max_length=50, null=True, blank=True)
+    estimated_cooking_time = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
+    def __str__(self):
+        return self.name
