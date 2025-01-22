820d6b1a8f2f57bd434d94deb1df1d86c7127115
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-23 02:48:08 +0330

    feat: add admin register for "Order" and "OrderItem" class models

--- coffee_shop/admin.py
+++ coffee_shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from coffee_shop.models import *
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "table", "menu_items", "status", "timestamp"]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "menu_item", "timestamp"]
