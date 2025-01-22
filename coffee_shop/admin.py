00976825b5d66efbbabd1096cad5fbd20cb74173
darya84 <daryalcd1384@gmail.com>
2025-01-23 01:59:05 +0330

    feat: admin section created for menu class.

--- coffee_shop/admin.py
+++ coffee_shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from coffee_shop.models import MenuItem
+
+
 # Register your models here.
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'price', 'category', 'discount', 'description', 'serving_time_period', 'estimated_cooking_time']
