8ba52cf048465d81cee4a00fbb03a1fc34fb56e3
Zahed <88390908+Zahedhosseini65@users.noreply.github.com>
2025-01-22 20:55:40 +0330

    feat:add class Receipt

--- coffee_shop/admin.py
+++ coffee_shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Receipt
 
-# Register your models here.
+
+admin.site.register(Receipt)
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('order', 'total_price', 'final_price ', 'timestamp')
+admin.site.register(Receipt, ReceiptAdmin)
--- coffee_shop/models.py
+++ coffee_shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Receipt(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    final_price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
