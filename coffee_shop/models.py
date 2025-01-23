761e03ea9bf62bff73e27099955cda49e15f45c4
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-23 09:30:20 +0330

    feat: update "Order" and "OrderItem" class models

--- coffee_shop/models.py
+++ coffee_shop/models.py
@@ -8,8 +8,14 @@ class Order(models.Model):
     status = models.CharField(max_length=20, default='Pending')
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user.username} {self.table} {self.menu_items} {self.status} {self.timestamp}"
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.order} {self.menu_item} {self.quantity}"
