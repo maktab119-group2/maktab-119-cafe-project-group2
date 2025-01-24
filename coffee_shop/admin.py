9e14578a52ba61de4738cb5f638a1b9ae927fb3e
radinettehad <radin_ettehad@yahoo.com>
2025-01-24 09:33:43 +0330

    feat:add new models of the payment plus create new admin.py

--- coffee_shop/admin.py
+++ coffee_shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from coffee_shop.models import Payment
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'payment_method', 'timestamp')
 
-# Register your models here.
--- coffee_shop/models.py
+++ coffee_shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment for Receipt {self.receipt.id}"
