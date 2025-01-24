73c8d1c06657cb602aa9dbc85deb14879ded3a7b
radinettehad <radin_ettehad@yahoo.com>
2025-01-24 09:41:03 +0330

    feat:add new view of the cashier_dashboard

--- coffee_shop/views.py
+++ coffee_shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from coffee_shop.models import Order
 
-# Create your views here.
+def cashier_dashboard(request):
+    orders = Order.objects.filter(status='Pending')
+    return render(request, 'cafe/dashboard.html', {'orders': orders})
+
+
+def order_detail(request, order_id):
+    order = Order.objects.get(id=order_id)
+    return render(request, 'cafe/order_detail.html', {'order': order})
