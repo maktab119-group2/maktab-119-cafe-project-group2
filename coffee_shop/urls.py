60147139c5f5830d7b60ad0f97a9b1f31b3832d3
darya84 <daryalcd1384@gmail.com>
2025-01-29 20:53:41 +0330

    feat: add login page for cashier panel.

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', login_cashier, name='login'),
     path('menu/', menu, name='menu'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
--- coffee_shop/views.py
+++ coffee_shop/views.py
@@ -3,6 +3,8 @@ from .models import MenuItem, Category, Order, Receipt
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth import authenticate, login
 from .forms import MenuItemForm
+from django.contrib import messages
+
 
 
 
@@ -63,20 +65,20 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
 
-
-def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = authenticate(request, email=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-    else:
-        form = UserLoginForm()
-    return render(request, 'login.html', {'form': form})  
+#
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, email=email, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+#     else:
+#         form = UserLoginForm()
+#     return render(request, 'login.html', {'form': form})
 
 
 def cashier_dashboard(request):
@@ -87,3 +89,18 @@ def cashier_dashboard(request):
 def order_detail(request, order_id):
     order = Order.objects.get(id=order_id)
     return render(request, 'cafe/order_detail.html', {'order': order})
+#
+#
+def login_cashier(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/admin/')  # تغییر مسیر بعد از ورود موفق
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'login.html')
