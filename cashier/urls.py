4851fda8ad7f96a6357ea023e251050b81d9d1df
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-02-03 15:15:32 +0330

    feat: updated login url

--- cashier/urls.py
+++ cashier/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('add-menu-item/', AddMenuItemView.as_view(), name='add_menu_item'),
     path('edit-menu-item/<int:item_id>/', EditMenuItemView.as_view(), name='edit_menu_item'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    # path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginCashierView.as_view(), name='login_cashier'),
     path('cashier-dashboard/', CashierDashboardView.as_view(), name='cashier_dashboard'),
     path('order-detail/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
     path('login-cashier/', LoginCashierView.as_view(), name='login_cashier'),
