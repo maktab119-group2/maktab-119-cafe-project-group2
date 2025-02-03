531fb1f5737a31d8d9993c4690bf74c0eb358134
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-02-03 15:37:03 +0330

    feat: registered models

--- cashier/admin.py
+++ cashier/admin.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.admin import UserAdmin
+from cashier.models import CashierUser, CustomUser
 from django.contrib import admin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_superuser", "is_active", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff", "is_superuser", "is_active", "groups",
+                "user_permissions"),
+        }),
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)
+
+
+class CashierUserAdmin(UserAdmin):
+    model = CashierUser
+    list_display = ("email", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_superuser", "is_staff", "is_active", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff", "is_superuser", "is_active", "groups",
+                "user_permissions"),
+        }),
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_staff=True, is_superuser=False)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CashierUser, CashierUserAdmin)
