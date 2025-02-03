82a02e7fb0941a5ed9406268d3f669dc9aa863b5
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-02-03 15:15:11 +0330

    feat: added "AUTH_USER_MODEL = "cashier.CustomUser"" for authenticate custom users

--- src/settings.py
+++ src/settings.py
@@ -153,3 +153,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "cashier.CustomUser"
+
