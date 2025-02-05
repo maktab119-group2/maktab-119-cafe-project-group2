859a1e7b53d7ae7932672eaa3e4ef630528d82d3
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-02-05 17:21:34 +0330

    feat: added some urls for orders list and update order list

--- cashier/urls.py
+++ cashier/urls.py
@@ -6,12 +6,9 @@ urlpatterns = [
     path('item_detail/<int:item_id>/', ItemDetailView.as_view(), name='item_detail'),
     path('add-menu-item/', AddMenuItemView.as_view(), name='add_menu_item'),
     path('edit-menu-item/<int:item_id>/', EditMenuItemView.as_view(), name='edit_menu_item'),
-    # path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginCashierView.as_view(), name='login_cashier'),
     path('cashier-dashboard/', CashierDashboardView.as_view(), name='cashier_dashboard'),
-    path('order-detail/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
+    path('order/update/<int:pk>/', OrderUpdateView.as_view(), name='order_update'),
     path('login-cashier/', LoginCashierView.as_view(), name='login_cashier'),
-    path("tables/", TableListView.as_view(), name="table_list"),
-    path("tables/<int:table_id>/", TableDetailView.as_view(), name="table_detail"),
 
 ]
